b01effee3609feeb09ff5e44ce97c92e52cecbaf
nopara73 <adam.ficsor73@gmail.com>
2024-09-20 14:18:15 +0200

    Add  index, configure static file serving

--- LongevityWorldCup.Website/Program.cs
+++ LongevityWorldCup.Website/Program.cs
@@ -5,9 +5,30 @@ namespace LongevityWorldCup.Website
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            // Enable default file serving (index.html) and static file serving
+            app.UseDefaultFiles();  // <-- Add this line to serve default files like index.html
+            app.UseStaticFiles();   // <-- Make sure this comes after UseDefaultFiles()
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+             name: "default",
+             pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
