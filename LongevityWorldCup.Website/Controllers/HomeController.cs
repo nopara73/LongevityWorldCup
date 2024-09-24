b20428972a9eb838c5c077155f1ddb9dc1c4c84a
nopara73 <adam.ficsor73@gmail.com>
2024-09-24 13:18:57 +0200

    add controller

---
+++ LongevityWorldCup.Website/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LongevityWorldCup.Website.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("join")]
+        public IActionResult JoinGame()
+        {
+            // Return the static index.html file from wwwroot
+            // This is just a test
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/html");
+        }
+    }
+}
