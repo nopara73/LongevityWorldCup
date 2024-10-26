7a187334af43daf342e90bc17cda2d55ee004710
nopara73 <adam.ficsor73@gmail.com>
2024-10-26 09:57:00 +0200

    get flags from C#

---
+++ LongevityWorldCup.Website/Controllers/DataController.cs
@@ -0,0 +1,16 @@
+﻿using LongevityWorldCup.Website.Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LongevityWorldCup.Website.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+        [HttpGet("flags")]
+        public IActionResult GetFlags()
+        {
+            return Ok(Flags.Flag);
+        }
+    }
+}
