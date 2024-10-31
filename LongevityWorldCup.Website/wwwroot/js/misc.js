ca82d7b919d65a3b3b49b5d097be44629e1701db
nopara73 <adam.ficsor73@gmail.com>
2024-10-31 10:56:19 +0100

    media contact

---
+++ LongevityWorldCup.Website/wwwroot/js/misc.js
@@ -0,0 +1,60 @@
+﻿function getIcon(link) {
+    // Normalize the link to lowercase for case-insensitive matching
+    const normalizedLink = link.toLowerCase();
+
+    // Mapping of link identifiers to Font Awesome icon classes
+    const iconMap = {
+        email: '<i class="fas fa-envelope"></i>', // Email icon
+        'facebook.com': '<i class="fab fa-facebook"></i>',
+        'twitter.com': '<i class="fab fa-twitter"></i>',
+        'linkedin.com': '<i class="fab fa-linkedin"></i>',
+        'instagram.com': '<i class="fab fa-instagram"></i>',
+        'youtube.com': '<i class="fab fa-youtube"></i>',
+        'github.com': '<i class="fab fa-github"></i>',
+        'gitlab.com': '<i class="fab fa-gitlab"></i>',
+        'bitbucket.org': '<i class="fab fa-bitbucket"></i>',
+        'paypal.com': '<i class="fab fa-paypal"></i>',
+        'stackoverflow.com': '<i class="fab fa-stack-overflow"></i>',
+        'medium.com': '<i class="fab fa-medium"></i>',
+        'reddit.com': '<i class="fab fa-reddit"></i>',
+        'tumblr.com': '<i class="fab fa-tumblr"></i>',
+        'pinterest.com': '<i class="fab fa-pinterest"></i>',
+        'snapchat.com': '<i class="fab fa-snapchat"></i>',
+        'whatsapp.com': '<i class="fab fa-whatsapp"></i>',
+        'telegram.org': '<i class="fab fa-telegram"></i>',
+        'discord.com': '<i class="fab fa-discord"></i>',
+        'slack.com': '<i class="fab fa-slack"></i>',
+        'dribbble.com': '<i class="fab fa-dribbble"></i>',
+        'behance.net': '<i class="fab fa-behance"></i>',
+        'flickr.com': '<i class="fab fa-flickr"></i>',
+        'spotify.com': '<i class="fab fa-spotify"></i>',
+        'vimeo.com': '<i class="fab fa-vimeo"></i>',
+        'tiktok.com': '<i class="fab fa-tiktok"></i>',
+        'skype.com': '<i class="fab fa-skype"></i>',
+        'wordpress.org': '<i class="fab fa-wordpress"></i>',
+        'stackoverflow.com': '<i class="fab fa-stack-overflow"></i>',
+        'amazon.com': '<i class="fab fa-amazon"></i>',
+        'google.com': '<i class="fab fa-google"></i>',
+        'apple.com': '<i class="fab fa-apple"></i>',
+        'microsoft.com': '<i class="fab fa-microsoft"></i>',
+        'spotify.com': '<i class="fab fa-spotify"></i>',
+        'npmjs.com': '<i class="fab fa-npm"></i>',
+        'bitly.com': '<i class="fas fa-link"></i>', // Bitly doesn't have a specific icon
+        // Add more mappings as needed
+    };
+
+    // Check for email first
+    if (normalizedLink.includes('@')) {
+        return iconMap.email;
+    }
+
+    // Iterate through the iconMap to find a matching domain
+    for (const [key, icon] of Object.entries(iconMap)) {
+        if (normalizedLink.includes(key)) {
+            return icon;
+        }
+    }
+
+    // Return a generic link icon if no match is found
+    return '<i class="fas fa-link"></i>';
+}
