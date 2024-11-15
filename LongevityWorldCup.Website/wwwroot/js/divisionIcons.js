c51ac32858c141eb20724dab9d344dd16678b3c2
nopara73 <adam.ficsor73@gmail.com>
2024-11-15 11:47:44 +0100

    division icon

---
+++ LongevityWorldCup.Website/wwwroot/js/divisionIcons.js
@@ -0,0 +1,10 @@
+﻿window.GetDivisionIcon = function (division) {
+    const divisionIcons = {
+        "men's": "💪",
+        "women's": "👠",
+        "open": "🦾"
+    };
+
+    // Return the icon if it exists, or an empty string if not
+    return divisionIcons[division.toLowerCase()] || '';
+}
