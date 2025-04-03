b6ecd56a2a2e41bf9d3ef279904e485dd2c883cf
nopara73 <adam.ficsor73@gmail.com>
2025-04-03 15:29:52 +0200

    add 404 herold

---
+++ LongevityWorldCup.Website/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LongevityWorldCup.Website.Controllers
+{
+    [ApiController]
+    [Route("error")]
+    public class ErrorController() : Controller
+    {
+        [HttpGet("404")]
+        public IActionResult NotFoundPage()
+        {
+            return Redirect("/error/404.html");
+        }
+    }
+}
