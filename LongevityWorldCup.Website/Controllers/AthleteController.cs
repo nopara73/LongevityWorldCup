152c0dd771af1ede39eed41dd94eb247416b7fd2
nopara73 <adam.ficsor73@gmail.com>
2025-04-07 15:35:52 +0200

    Improve athlete URL

---
+++ LongevityWorldCup.Website/Controllers/AthleteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LongevityWorldCup.Website.Controllers
+{
+    [Route("athlete")]
+    public class AthleteController : Controller
+    {
+        [HttpGet("{athleteName}")]
+        public IActionResult RedirectToHome(string athleteName)
+        {
+            return Redirect($"/?athlete={athleteName}");
+        }
+    }
+}
