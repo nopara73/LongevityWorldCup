a5eba53d0a49a77509aa76cc3c0de40702515bfd
nopara73 <adam.ficsor73@gmail.com>
2025-04-11 14:33:04 +0200

    leagueicons to newfile

---
+++ LongevityWorldCup.Website/wwwroot/js/leagueIcons.js
@@ -0,0 +1,24 @@
+﻿window.TryGetDivisionIcon = function (division) {
+    const divisionIcons = {
+        "men's": "💪",
+        "women's": "👠",
+        "open": "🦾"
+    };
+
+    // Return the icon if it exists, or an empty string if not
+    return divisionIcons[division.toLowerCase()] || '';
+}
+
+window.TryGetGenerationIcon = function (generation) {
+    const generationIcons = {
+        "silent generation": "📻",
+        "baby boomers": "📺",
+        "gen x": "🖥️",
+        "millennials": "💻",
+        "gen z": "📱",
+        "gen alpha": "🚀"
+    };
+
+    // Return the icon if it exists, or an empty string if not
+    return generationIcons[generation.toLowerCase()] || '';
+}
