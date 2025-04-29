401bd1779ea879c506f617f4bdb17f8ab48cb138
nopara73 <adam.ficsor73@gmail.com>
2025-04-29 11:37:02 +0200

    consolidate proof upload instructions

---
+++ LongevityWorldCup.Website/wwwroot/js/proof-helpers.js
@@ -0,0 +1,7 @@
+﻿window.getMainProofInstructionsInnerHTML = function () {
+    return "Upload <strong>image proofs</strong> of your biomarkers (e.g., screenshots of PDF results or photos of physical documents)";
+}
+
+window.getSubProofInstructionsInnerHTML = function () {
+    return "These images will be <strong>public</strong>, so you're encouraged to censor any irrelevant information.";
+}
