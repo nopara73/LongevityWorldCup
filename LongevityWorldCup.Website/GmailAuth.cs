ad328b8c79b2954a6b553233963f0705b48cf4a4
nopara73 <adam.ficsor73@gmail.com>
2025-05-24 15:10:07 +0200

    Implemnet google auth. P.S.: FUCK GOOGLE

---
+++ LongevityWorldCup.Website/GmailAuth.cs
@@ -0,0 +1,27 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+namespace LongevityWorldCup.Website
+{
+    public static class GmailAuth
+    {
+        private static readonly string[] Scopes = { "https://mail.google.com/" };
+        private const string CredsFile = "smtp-credentials.json";
+        private const string TokenStore = "smtp-token.json";
+
+        public static UserCredential GetCredential()
+        {
+            using var stream = File.OpenRead(CredsFile);
+            var secrets = GoogleClientSecrets.FromStream(stream).Secrets;
+            return GoogleWebAuthorizationBroker.AuthorizeAsync(
+                secrets,
+                Scopes,
+                "user",
+                CancellationToken.None,
+                new FileDataStore(TokenStore, true)
+            ).Result;
+        }
+    }
+}
