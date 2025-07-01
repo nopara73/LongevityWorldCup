5122491a8454fb33b171729974ebc50f2ce16957
nopara73 <adam.ficsor73@gmail.com>
2025-07-01 14:37:29 +0200

    add guess controller

---
+++ LongevityWorldCup.Website/Controllers/GuessController.cs
@@ -0,0 +1,18 @@
+﻿using LongevityWorldCup.Website.Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LongevityWorldCup.Website.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuessController(AthleteDataService svc) : Controller
+    {
+        private readonly AthleteDataService _svc = svc;
+
+        [HttpPost("athlete-age")]
+        public IActionResult PostAthleteAgeGuess(string athleteName, int ageGuess)
+        {
+            return Ok();
+        }
+    }
+}
