f9ef772cfb39243bcaea07bff8b66910690cf2b0
nopara73 <adam.ficsor73@gmail.com>
2025-08-12 13:57:32 +0200

    finish media kit

---
+++ LongevityWorldCup.Website/Controllers/MediaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LongevityWorldCup.Website.Controllers
+{
+    [Route("media")]
+    public class MediaController : Controller
+    {
+        [HttpGet("")]
+        [HttpGet("/")]
+        public IActionResult RedirectToMedia()
+        {
+            return Redirect("/misc-pages/media.html");
+        }
+    }
+}
