d866b4a507bba0ecb00495cd966245eda24d83c4
nopara73 <adam.ficsor73@gmail.com>
2026-02-07 16:33:53 +0100

    namings

--- LongevityWorldCup.Website/wwwroot/js/bortz-age.js
+++ LongevityWorldCup.Website/wwwroot/js/bortz-age.js
@@ -15,9 +15,9 @@ window.BortzAge.features = [
     { id: 'cholesterol', name: 'Total Cholesterol', mean: 5.6177437, baaCoeff: -0.0805656, isLog: false },
     { id: 'creatinine', name: 'Creatinine', mean: 71.565605, baaCoeff: -0.01095746, isLog: false },
     { id: 'cystatin_c', name: 'Cystatin C', mean: 0.900946, baaCoeff: 1.859556436, isLog: false },
-    { id: 'hba1c', name: 'HbA1c', mean: 35.4785711, baaCoeff: 0.018116675, isLog: false },
-    { id: 'crp', name: 'C-reactive protein (hsCRP)', mean: 0.3003624, baaCoeff: 0.079109916, isLog: true },
-    { id: 'ggt', name: 'Gamma-glutamyl transferase', mean: 3.3795613, baaCoeff: 0.265550311, isLog: true },
+    { id: 'hba1c', name: 'Hemoglobin A1c (HbA1c)', mean: 35.4785711, baaCoeff: 0.018116675, isLog: false },
+    { id: 'crp', name: 'C-Reactive Protein (CRP)', mean: 0.3003624, baaCoeff: 0.079109916, isLog: true },
+    { id: 'ggt', name: 'Gamma-Glutamyl Transferase (GGT)', mean: 3.3795613, baaCoeff: 0.265550311, isLog: true },
     { id: 'rbc', name: 'Red blood cell count', mean: 4.4994648, baaCoeff: -0.204442153, isLog: false },
     { id: 'mcv', name: 'Mean corpuscular volume', mean: 91.9251099, baaCoeff: 0.017165356, isLog: false },
     { id: 'rdw', name: 'Red cell distribution width', mean: 13.4342296, baaCoeff: 0.202009895, isLog: false, cap: 11.4, capMode: 'floor' },
@@ -25,11 +25,11 @@ window.BortzAge.features = [
     { id: 'neutrophil_count', name: 'Neutrophils', mean: 4.1849454, baaCoeff: 0.06679092, isLog: false },
     { id: 'lymphocyte_percentage', name: 'Lymphocytes (%)', mean: 28.5817604, baaCoeff: -0.0108158, isLog: false, cap: 60, capMode: 'ceiling' },
     { id: 'alt', name: 'Alanine aminotransferase', mean: 3.077868, baaCoeff: -0.312442261, isLog: true },
-    { id: 'shbg', name: 'SHBG', mean: 3.8202787, baaCoeff: 0.292323186, isLog: true },
+    { id: 'shbg', name: 'Sex Hormone-Binding Globulin (SHBG)', mean: 3.8202787, baaCoeff: 0.292323186, isLog: true },
     { id: 'vitamin_d', name: 'Vitamin D (25-OH)', mean: 3.6052878, baaCoeff: -0.265467867, isLog: true },
     { id: 'glucose', name: 'Glucose', mean: 4.9563054, baaCoeff: 0.032171478, isLog: false, cap: 4.44, capMode: 'floor' },
     { id: 'mch', name: 'Mean corpuscular hemoglobin', mean: 31.8396206, baaCoeff: 0.02746487, isLog: false },
-    { id: 'apoa1', name: 'Apolipoprotein A1', mean: 1.5238771, baaCoeff: -0.185139395, isLog: false }
+    { id: 'apoa1', name: 'Apolipoprotein A1 (ApoA1)', mean: 1.5238771, baaCoeff: -0.185139395, isLog: false }
 ];
 
 // Backward compatibility: alias for form code that expects biomarkers
