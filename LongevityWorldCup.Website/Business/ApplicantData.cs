4363d10549b146d93ade8a771b1d19f716919df4
nopara73 <adam.ficsor73@gmail.com>
2026-02-09 09:27:32 +0100

    committing hope i dont need to roll it back

--- LongevityWorldCup.Website/Business/ApplicantData.cs
+++ LongevityWorldCup.Website/Business/ApplicantData.cs
@@ -11,7 +11,8 @@ namespace LongevityWorldCup.Website.Business
         public string? Why { get; set; }
         public string? MediaContact { get; set; }
         public string? AccountEmail { get; set; }
-        public string? ChronoBioDifference { get; set; }
+        public string? ChronoPhenoDifference { get; set; }
+        public string? ChronoBortzDifference { get; set; }
         public string? PersonalLink { get; set; }
         public string? ProfilePic { get; set; } // Base64 string
         public List<string>? ProofPics { get; set; } // List of Base64 strings
