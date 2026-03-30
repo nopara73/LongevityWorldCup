a3d6127a3fd86f01bb387e8cb0c14cda424f857c
RolandUI <soos.roland93@gmail.com>
2026-03-30 10:27:59 +0200

    updated workflow

--- LongevityWorldCup.Website/Program.cs
+++ LongevityWorldCup.Website/Program.cs
@@ -256,6 +256,7 @@ namespace LongevityWorldCup.Website
                 FacebookAppId = "",
                 FacebookAppSecret = "",
                 FacebookPageId = "",
+                FacebookUserAccessToken = "",
                 FacebookPageAccessToken = ""
             };
 
