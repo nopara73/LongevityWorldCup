52924b679ff1cf705085c66a43fd725b67b4a183
nopara73 <adam.ficsor73@gmail.com>
2026-07-10 09:21:37 +0200

    Merge duplicate FAQ link

--- LongevityWorldCup.Tests/HomepageLinkAccessibilityTests.cs
+++ LongevityWorldCup.Tests/HomepageLinkAccessibilityTests.cs
@@ -15,4 +15,16 @@ public sealed class HomepageLinkAccessibilityTests
         Assert.Contains("<i class=\"fas fa-book-open\" aria-hidden=\"true\"></i> History of longevity as a sport", html);
         Assert.DoesNotContain("text-decoration: none;\"><i class=\"fas fa-book-open\"", html);
     }
+
+    [Fact]
+    public async Task ExtendedFaqCallToAction_IsOneNamedLink()
+    {
+        using var factory = new TestWebApplicationFactory();
+        using var client = factory.CreateClient();
+
+        var html = await client.GetStringAsync("/");
+
+        Assert.Contains("<a href=\"/ruleset#faq\"><i class=\"fas fa-circle-question\" aria-hidden=\"true\"></i> Extended FAQ</a>", html);
+        Assert.DoesNotContain("homepage-faq-icon-link", html);
+    }
 }
